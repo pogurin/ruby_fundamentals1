2a129ab277aa0351fcc08c5cba9b55f441ab3378
MitsuyoshiToguri <seiun340066@gmail.com>
2014-10-21 12:21:08 -0400

    This is my second exercise. ver1.

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+price = 55
+puts "price is #{price.to_i*1.3.to_f}!" 
+#This is tip task.
+
+puts "7839".to_i + 7839
+# I have changed my string to integer.
+
+puts 45628.to_i * 7839.to_i 
+#The result of 45628 multiplied by 7839
+
+puts (true && false) || (false && true) || !(false && false)
+# It will be true.
