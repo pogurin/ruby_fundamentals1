a14a134e1e21a1a316730cd0e132f194e45012b9
MitsuyoshiToguri <seiun340066@gmail.com>
2014-10-21 12:25:01 -0400

    This is my practice 3 ver1.

---
+++ exercise3.rb
@@ -0,0 +1,10 @@
+name = "Tom"
+puts "Hi #{name}!" 
+
+puts "How old are you?"
+age = 42
+puts "Hi I am #{age} years old!"
+puts "When did you born ?"
+puts 2014 - age.to_i 
+
+# Asking age also asnwer the year when he/she borned.
